f65cbe5738b95514bea7621374cca1ebc909e40b
Brian-M-Code <mutuma.brian@yahoo.com>
2019-11-01 12:31:26 +0300

    added hover function in the script file

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ $(document).ready(function () {
         $("#hide2").slideToggle("slow");
         $("#icon2").slideToggle("slow");
     })
+    $("#col_1").hover(function () {
+        $("#work1").fadeTo("slow", 0.6);
+        $("#proj1").fadeTo("slow", 1);
+    }, function () {
+        $("#work1").fadeTo("slow", 1);
+        $("#proj1").fadeTo("slow", 0);
+    });
+    $("#col_2").hover(function () {
+        $("#work2").fadeTo("slow", 0.6);
+        $("#proj2").fadeTo("slow", 1);
+    }, function () {
+        $("#work2").fadeTo("slow", 1);
+        $("#proj2").fadeTo("slow", 0);
+    });
+    }, function () {
+        $("#img_3").fadeTo("slow", 1);
+        $("#proj3").fadeTo("slow", 0);
+    });
+    $("#col_4").hover(function () {
+        $("#work4").fadeTo("slow", 0.6);
+        $("#proj4").fadeTo("slow", 1);
+    }, function () {
+        $("#work4").fadeTo("slow", 1);
+        $("#proj4").fadeTo("slow", 0);
+    });
+   
