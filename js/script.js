cab1c2f7fc6dfb8ff999ea2149ed71f489e3cb35
Brian-M-Code <mutuma.brian@yahoo.com>
2019-11-01 10:36:44 +0300

    Added the script file

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".clickable").click(function () {
+        $("#hide").slideToggle("slow");
+        $("#icon").slideToggle("slow");
+    })
+    $(".clickable1").click(function () {
+        $("#hide1").slideToggle("slow");
+        $("#icon1").slideToggle("slow");
+    })
+    $(".clickable2").click(function () {
+        $("#hide2").slideToggle("slow");
+        $("#icon2").slideToggle("slow");
+    })
